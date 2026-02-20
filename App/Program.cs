bd94922f229790a8cabb9c20fe1cdf64674d6f7b
Boris Shesterikov <boris20033@gmail.com>
2026-02-20 23:47:33 +0400

    Add ASP.NET fraemwork.
    Add ASP.NET fraemwork. Add DI, Hosting, Mapper, Loging.

--- App/Program.cs
+++ App/Program.cs
@@ -1,13 +1,50 @@
+using Microsoft.EntityFrameworkCore;
 using AsyncWarehouse.Application;
+using AsyncWarehouse.Application.AutoMapper;
+using AsyncWarehouse.Infrastructure;
+using AutoMapper;
 
-var deliveryServices = new List<IDeliveryService>
+var builder = WebApplication.CreateBuilder(args);
+
+// Выведет в консоль текущую среду
+Console.WriteLine($"Current Environment: {builder.Environment.EnvironmentName}");
+
+// Add support of Controllers
+builder.Services.AddControllers();
+
+var mapperConfig = new MapperConfiguration(config => 
+    {
+        config.AddProfile(new MappingProfile());     // Профиль из Application
+    },
+    LoggerFactory.Create(b => b.AddConsole()));
+IMapper? mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+
+builder.Services.AddKeyedScoped<IDeliveryService, TruckDelivery>("truck");
+builder.Services.AddKeyedScoped<IDeliveryService, ShipDelivery>("ship");
+builder.Services.AddKeyedScoped<IDeliveryService, DroneDelivery>("drone");
+
+// Add Swagger and documentation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Connect to Postgresql
+builder.Services.AddDbContext<ApplicationContext>(options =>
 {
-    new DroneDelivery(),
-    new TruckDelivery(),
-    new ShipDelivery()
-};
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
-var warehouse = new Warehouse(deliveryServices);
-var cts = new CancellationTokenSource();
+builder.Services.AddScoped<WarehouseService>();
 
-await warehouse.ProcessIncomingOrdersAsync(cts.Token);
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
